f1d87b0123ce90f0db363c8d07ce9027abd2e82d
raushan <kraushan022@gmail.com>
2025-05-15 12:40:07 +0530

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="font-bold bg-amber-300 border border-black">
+        Namaste Netflix
+      </h1>
+    </>
+  );
+}
+
+export default App;
