6796b8231dd38348274b77d87cad0e4e5a3c466a
raushan <kraushan022@gmail.com>
2025-05-22 12:43:56 +0530

    added trasition transform on cards hover

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,8 +4,12 @@ import { IMG_CDN_URL } from "../utils/constants";
 const MovieCard = ({ posterPath }) => {
   if (!posterPath) return null;
   return (
-    <div className="w-36 md:w-48 pr-3">
-      <img alt="movie_card" src={IMG_CDN_URL + posterPath} />
+    <div className="w-36 md:w-48 pr-3 ">
+      <img
+        alt="movie_card"
+        src={IMG_CDN_URL + posterPath}
+        className="transform transition-transform duration-300 hover:scale-112"
+      />
     </div>
   );
 };
